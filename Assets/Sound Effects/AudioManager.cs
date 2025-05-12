08a03a987bf0051e00f504307d47e1fce02753e3
Khoa Hoang <79607148+khoang9477@users.noreply.github.com>
2025-05-11 19:05:38 -0700

    Add AudioManager Scripts and Prefabs

---
+++ Assets/Sound Effects/AudioManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    public AudioSource sfxSource;
+
+    [Header("Player")]
+    public AudioClip walk;
+    public AudioClip run;
+    public AudioClip dash;
+    public AudioClip slide;
+    public AudioClip vault;
+    public AudioClip climbLadder;
+    public AudioClip sprint;
+    public AudioClip death;
+
+    [Header("Combat")]
+    public AudioClip shoot;
+    public AudioClip reload;
+    public AudioClip grenadeThrow;
+    public AudioClip grenadeExplosion;
+    public AudioClip bulletHit;
+    public AudioClip bulletImpact;
+
+    [Header("Extras")]
+    public AudioClip wallBreak;
+    public AudioClip menuSelect;
+
+    void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    void PlaySound(AudioClip clip)
+    {
+        if (clip != null)
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+}
